47446f57abb21f5ef8c906ebc3978339f9592e6e
kamilyfatec <kamily.simeao@fatec.sp.gov.br>
2024-03-25 12:27:15 -0300

    Aula2503

---
+++ StockApp/StockApp.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Infra.Data.Context
+{
+    internal class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
