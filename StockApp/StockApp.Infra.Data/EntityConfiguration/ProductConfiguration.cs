dcbb8501cf50684b2318736ead3588df4ead12aa
kamilyfatec <kamily.simeao@fatec.sp.gov.br>
2024-04-01 12:36:10 -0300

    Aula0104-configuracoes do ef e repositorios categorias

---
+++ StockApp/StockApp.Infra.Data/EntityConfiguration/ProductConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StockApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Infra.Data.EntityConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(d => d.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(100).IsRequired();
+            builder.Property(c => c.Price).HasPrecision(10,2);
+            builder.HasOne(t => t.Category).WithMany(p => p.Products).HasForeignKey(t => t.CategoryId);
+
+            
+        }
+    }
+}
